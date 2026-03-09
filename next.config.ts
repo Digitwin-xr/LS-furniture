959ca5c131895a9977bfe7e744070b78328a9769
Digitwin xr <phygitalconcepts@gmail.com>
2026-03-09 18:25:16 +0200

    build: poke vercel triggering build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: 'standalone', // Triggering production rebuild
 };
 
 export default nextConfig;
